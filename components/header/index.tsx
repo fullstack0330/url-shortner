ccd42be68605679c2284ed9e97bcd6b1dfb16ced
Irontiger0330 <114550590+Irontiger0330@users.noreply.github.com>
2023-10-19 18:38:01 +0900

    fix

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Github } from "lucide-react";
+
 function Header() {
   return (
     <header className="container flex flex-row items-center justify-between">
       <h1 className="text-2xl font-semibold">Shortly</h1>
       <a
-        href="https://github.com/phantomknight287/url-shortner"
+        href="https://github.com/fullstack0330/url-shortner"
         target="_blank"
       >
         <Github className="w-6 h-6" />
